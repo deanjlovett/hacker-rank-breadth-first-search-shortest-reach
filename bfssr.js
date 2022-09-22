fac74b5006cc200d085870442d7d2ca13b49c749
Dean Lovett <dean.lovett@gmail.com>
2022-09-22 15:00:48 -0700

    add another attempt at solution

--- bfssr.js
+++ bfssr.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const { ChildProcess } = require('child_process');
 const fs = require('fs');
+const { getSystemErrorMap } = require('util');
 
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
@@ -36,72 +38,250 @@ function readLine() {
  *  every edge counts as 6 (six)
  */
 
+const dline = '========================';
+const sline = '------------------------';
+
+const isConLog = true;
+const isDebugLog = false;
+
+function clog(...args){
+    if(isConLog)
+        console.log(...args);
+}
+function dlog(...args){
+    if(isDebugLog && isConLog)
+        console.log(...args);
+}
+
+function clogdline(){
+    clog(dline);
+}
+function clogsline(){
+    clog(sline);
+}
+function dlogdline(){
+    dlog(dline);
+}
+function dlogsline(){
+    dlog(sline);
+}
+
 function bfs(n, m, edges, s) {
     // Write your code here
-    console.log(`function bfs(n:${n}, m:${m}, edges, s:${s}) {`);
-
-    console.log('========================');
-
-    console.log('.   nodes, n:',n);
-    console.log('.   edges, m:',m);
-    console.log('start, s:',s);
-    console.log('array of edges:');
-    console.log('[');
-    edges.forEach(e=>{
-        console.log('  [',e[0],',',e[1],']');
-    });
-    console.log(']');
     
-    let myMap = {};
-    let mySet = new Set();
+    let myMap = new Map();
+    let myNodeSet = new Set();
+
     edges.forEach((e,i,arr)=>{
-        myMap[''+e[1]] = ''+e[0];
-        mySet.add(e[0]);
-        mySet.add(e[1]);
-    })
-    let myList = [...mySet].sort((a,b)=>a-b);
-    console.log('myList:');
-    console.log(myList);
-    console.log('myMap:');
-    console.log(myMap);
+        let k = e[0];
+        let v = e[1];
+        myNodeSet.add(k);
+        myNodeSet.add(v);
+        for(let j=0;j<2;++j) {
+            if( myMap.has(k) ){
+                let it = myMap.get(k);
+                it.childs.add(v);
+            }else{
+                let tset = new Set();
+                tset.add(v);
+                let myObj = 
+                {
+                    key: k,
+                    childs: tset,
+                    dist:-1,
+                    chain: [],
+                    chainset: new Set(),
+                    isTop: false
+                };
+                myMap.set(k,myObj);
+            }
+            let tmp = k;
+            k = v;
+            v = tmp;
+        }
+    });
 
     let ret = [];
+    let wq = [];
+
+    let start = myMap.get(s);
+    start.isTop = true;
+    start.dist = 0;
+
+    myMap.set(s,start);
+    wq.push( start );
+
+    while(wq.length >0 ){
+        let w = wq.shift();// pop from front
+        if( myNodeSet.has(w.key) ) {
+            myNodeSet.delete(w.key);
+        }
+
+        let wl = [... w.childs]; //.sort((a,b)=>a-b); //<= may not need to sort
+        wl.forEach((e,i,arr)=>{
+            let echild = myMap.get(e);
+            if( echild.chainset.has(w.key)) return;
+            echild.chain.push( w.key );
+            if( echild.dist == -1 ) echild.dist = w.dist + 1;
+            if( !myNodeSet.has(e) ) return;
+            wq.push(echild);
+        });
+    }
     for(let i=1; i<=n; ++i){
-        console.log('loop, i:',i)
         if( i === s) {
-            console.log(`  i:${i} == s:${s}, skip, continue`);
             continue;
         }
-        if( ! mySet.has(i) ){
-            console.log(`  i:${i} not in set ${myList}, push(-1), continue`);
+        if( myNodeSet.has(i) ){
+            ret.push(-1);
+            continue;
+        } 
+        ret.push( 
+            !(myMap.has(i)) ? 
+            -1 : 
+            6 *  myMap.get(i).dist 
+        );
+    }
+    return ret;
+}
+
+function bfs_with_debug(n, m, edges, s) {
+    // Write your code here
+    clogdline();
+    clog(`function bfs(nodes:${n}, edges:${m}, edges, start${s}) {`);
+    clogsline();
+
+    clog('array of edges:');
+    clog('[');
+    edges.forEach(e=>{
+        clog('  [',e[0],',',e[1],']');
+    });
+    clog(']');
+    
+    let myMap = new Map();
+    let myNodeSet = new Set();
+
+    let up = 0;
+
+    // edges.forEach((e,i,arr)=>{
+    for( let i=0; i<edges.length; ++i){
+        let e = edges[i];
+        dlog(`  e:`, e)
+        // let up = e[0];
+        let k = e[0];
+        let v = e[1];
+        dlog(`  i:${i}`)
+        dlog(`    k:${k}, v:${v}`)
+        // dlog(`    myMap:`,myMap)
+        myNodeSet.add(k);
+        myNodeSet.add(v);
+        for(let j=0;j<2;++j) {
+            if( myMap.has(k) ){
+                dlog(`    myMap.has(k:${k}) == true`);
+                let it = myMap.get(k);
+                dlog(`    it:`);
+                dlog(it);
+                it.childs.add(v);
+                dlog(`    it.childs.add(v:${v})`);
+            }else{
+                dlog(`    myMap.has(k:${k}) == false`);
+                dlog(`    myMap.set(k:${k}, ...`);
+                let tset = new Set();
+                tset.add(v);
+                let myObj = 
+                {
+                    key: k,
+                    childs: tset,
+                    dist:-1,
+                    chain: [],
+                    chainset: new Set(),
+                    isTop: false
+                };
+                dlog(`       ${JSON.stringify(myObj)}  )`);
+                myMap.set(k,myObj);
+            }
+            let tmp = k;
+            k = v;
+            v = tmp;
+        }
+    }
+    // });
+
+    let ret = [];
+    let wq = [];
+    dlog(`looking for start:${s} in myMap`)
+    dlog(`myMap.has(s) === ${myMap.has(s)}`)
+    let start = myMap.get(s);
+    start.isTop = true;
+    start.dist = 0;
+    // since start obj changed put it back in the map
+    // may not be needed
+    myMap.set(s,start);
+    wq.push( start );
+    dlog(`start obj:`, JSON.stringify(start));
+
+    dlog(`object s added to work queue`);
+/*
+            {
+                key:e[0],
+                childs: new set(),
+                dist: 0,
+                chain: [],
+                isTop: false;
+            });
+ */
+    while(wq.length >0 ){
+        dlog(`inside while(wq.length:${wq.length} > 0)`)
+        let w = wq.shift();// pop from front
+        dlog(`  work: `, JSON.stringify(w));
+        if(w.dist === -1 ) dlog(`  error: work.dist == -1 *******`);
+        if( !myNodeSet.has(w.key) ) {
+            dlog('  error: myNodeSet doesnt have: ', w.key);
+        }else{
+            myNodeSet.delete(w.key);
+        }
+
+        let wl = [... w.childs]; //.sort((a,b)=>a-b); //<= may not need to sort
+        dlog(`  wl:`,wl);
+        dlog(`  entering wl.forEach(...), wl.length:`,wl.length);
+        wl.forEach((e,i,arr)=>{
+            dlog(`    wl: i`,i)
+            let echild = myMap.get(e);
+            if( echild.chainset.has(w.key)) return;
+            echild.chain.push( w.key );
+            if( echild.dist == -1 ) echild.dist = w.dist + 1;
+            if( !myNodeSet.has(e) ) return;
+            wq.push(echild);
+        });
+    }
+    dlog(`myNodeSet.length:`,myNodeSet.length);
+    dlog(`looping through nodes. node count:`,n);
+    for(let i=1; i<=n; ++i){
+        dlog('  loop, i:',i)
+        if( i === s) {
+            dlog(`    i:${i} == s:${s}, skip, continue`);
+            continue;
+        }
+        if( myNodeSet.has(i) ){
+            dlog(`    i:${i} not in set ${myNodeSet}, push(-1), continue`);
 
             ret.push(-1);
             continue;
         } 
-        let c = 0;
-        let t = ''+i;
-        console.log(`  starting while(t:${t} != s:${s}`);
-        while(t!=''+s){
-        console.log(`    inside while(t:${t} != s:${s}`);
+        // ret.push( !myMap.has(i) ? -1 : 6 *  myMap.get(i).dist );
 
-            if( ! myMap.hasOwnProperty(t) ){
-                console.log(`      myMap doesn't have key: ${t} ... will give up`);
-                break;
-            }
-            c += 6;
-            t = myMap[t];
-        }
-        if(t == ''+s){
-            console.log(`    t:${t} == s:${s}, ret.push(${t})`);
-            ret.push(c);
+        if( myMap.has(i)){
+            let node = myMap.get(i);
+            dlog(`node:`, node)
+            ret.push(6 * node.dist)
         }else{
-            console.log(`    t:${t} != s:${s}, ret.push(-1)`);
+            dlog(`no node with key:`, i);
+            dlog(`myMap`, myMap)
             ret.push(-1)
         }
     }
-    // console.log('------------------------');
-    // console.log(ret);
-    console.log('========================');
+    dlog('------------------------');
+    dlog(ret);
+    dlog('========================');
     return ret;
 }
 
@@ -137,14 +317,16 @@ function main() {
 
         const s = parseInt(readLine().trim(), 10);
 
+        // const result = bfs_with_debug(n, m, edges, s);
         const result = bfs(n, m, edges, s);
 
         ws.write(result.join(' ') + '\n');
 
         if( output_path === 'output.txt'){
-            console.log('result:');
-            console.log(result);
-            console.log();
+            clog('result:');
+            clog(result);
+            clog();
+            
         }
     }
     ws.end();
